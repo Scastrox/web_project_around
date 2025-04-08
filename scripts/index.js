d6fbd2a887de1aad4f7aec0e612699273f00febe
scastrox <scastrox87@gmail.com>
2025-04-08 01:51:52 -0600

    added popup feature submit form

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,28 @@
 const openFormButton = document.querySelector(".profile__info-edit-button");
 const popup = document.querySelector(".popup");
 const closeButton = popup.querySelector(".popup__close-button");
+const profileName = document.querySelector(".profile__info-name");
+const profileDescription = document.querySelector(".profile__info-description");
+const inputName = document.querySelector(".popup__form-field-name");
+const inputDescription = document.querySelector(
+  ".popup__form-field-description"
+);
+const form = document.querySelector(".popup__form");
 
 function toggleForm() {
+  inputName.value = profileName.textContent;
+  inputDescription.value = profileDescription.textContent;
   popup.classList.toggle("popup_visible");
 }
 
 openFormButton.addEventListener("click", toggleForm);
 closeButton.addEventListener("click", toggleForm);
+
+function handleProfileFormSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileDescription.textContent = inputDescription.value;
+  popup.classList.remove("popup_visible");
+}
+
+form.addEventListener("submit", handleProfileFormSubmit);
