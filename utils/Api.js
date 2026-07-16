12c0ff12960ed5a55367571ec858e6c715700d20
scastrox <scastrox87@gmail.com>
2026-07-15 20:42:51 -0600

    feat: add Api class for server requests

---
+++ utils/Api.js
@@ -0,0 +1,65 @@
+export default class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  _request(endpoint, options = {}) {
+    return fetch(`${this._baseUrl}${endpoint}`, {
+      headers: this._headers,
+      ...options,
+    }).then(this._checkResponse);
+  }
+
+  getUserInfo() {
+    return this._request("/users/me");
+  }
+
+  getInitialCards() {
+    return this._request("/cards");
+  }
+
+  getAppData() {
+    return Promise.all([this.getUserInfo(), this.getInitialCards()]);
+  }
+
+  updateUserInfo({ name, about }) {
+    return this._request("/users/me", {
+      method: "PATCH",
+      body: JSON.stringify({ name, about }),
+    });
+  }
+
+  updateAvatar(avatar) {
+    return this._request("/users/me/avatar", {
+      method: "PATCH",
+      body: JSON.stringify({ avatar }),
+    });
+  }
+
+  addCard({ name, link }) {
+    return this._request("/cards", {
+      method: "POST",
+      body: JSON.stringify({ name, link }),
+    });
+  }
+
+  deleteCard(cardId) {
+    return this._request(`/cards/${cardId}`, {
+      method: "DELETE",
+    });
+  }
+
+  changeLikeStatus(cardId, isLiked) {
+    return this._request(`/cards/${cardId}/likes`, {
+      method: isLiked ? "PUT" : "DELETE",
+    });
+  }
+}
