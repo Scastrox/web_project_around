a44914b95f1c23ad396c41221b3335842fa42631
scastrox <scastrox87@gmail.com>
2026-07-15 20:42:51 -0600

    refactor: adapt Card to server data with likes, owner check and callbacks

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,15 @@
 export default class Card {
-  constructor(data, templateSelector, handleCardClick) {
+  constructor(data, templateSelector, userId, { handleCardClick, handleLikeClick, handleDeleteClick }) {
     this._name = data.name;
     this._link = data.link;
+    this._id = data._id;
+    this._isLiked = data.isLiked;
+    this._ownerId = data.owner;
+    this._userId = userId;
     this._templateSelector = templateSelector;
     this._handleCardClick = handleCardClick;
+    this._handleLikeClick = handleLikeClick;
+    this._handleDeleteClick = handleDeleteClick;
   }
 
   _getTemplate() {
@@ -13,17 +19,27 @@ export default class Card {
       .querySelector(".gallery__card");
   }
 
-  _handleLikeClick() {
-    this._likeButton.classList.toggle("gallery__card-like_active");
+  getId() {
+    return this._id;
   }
 
-  _handleDeleteClick() {
+  isLiked() {
+    return this._isLiked;
+  }
+
+  setLikeStatus(isLiked) {
+    this._isLiked = isLiked;
+    this._likeButton.classList.toggle("gallery__card-like_active", isLiked);
+  }
+
+  removeCard() {
     this._element.remove();
+    this._element = null;
   }
 
   _setEventListeners() {
-    this._likeButton.addEventListener("click", () => this._handleLikeClick());
-    this._deleteButton.addEventListener("click", () => this._handleDeleteClick());
+    this._likeButton.addEventListener("click", () => this._handleLikeClick(this));
+    this._deleteButton.addEventListener("click", () => this._handleDeleteClick(this));
     this._cardImage.addEventListener("click", () => this._handleCardClick(this._name, this._link));
   }
 
@@ -36,6 +52,11 @@ export default class Card {
     this._cardImage.src = this._link;
     this._cardImage.alt = this._name;
     this._element.querySelector(".gallery__card-title").textContent = this._name;
+
+    this.setLikeStatus(this._isLiked);
+    if (this._ownerId !== this._userId) {
+      this._deleteButton.remove();
+    }
 
     this._setEventListeners();
     return this._element;
