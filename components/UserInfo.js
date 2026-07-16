193b1bda596fd49410b38055ee1c82c1fb9f373e
scastrox <scastrox87@gmail.com>
2026-07-15 20:43:21 -0600

    feat: extend UserInfo with avatar and user id handling

--- components/UserInfo.js
+++ components/UserInfo.js
@@ -1,7 +1,8 @@
 export default class UserInfo {
-  constructor({ nameSelector, jobSelector }) {
+  constructor({ nameSelector, jobSelector, avatarSelector }) {
     this._nameElement = document.querySelector(nameSelector);
     this._jobElement = document.querySelector(jobSelector);
+    this._avatarElement = document.querySelector(avatarSelector);
   }
 
   getUserInfo() {
@@ -11,8 +12,22 @@ export default class UserInfo {
     };
   }
 
-  setUserInfo({ name, job }) {
+  getUserId() {
+    return this._id;
+  }
+
+  setUserInfo({ name, about, avatar, _id }) {
     this._nameElement.textContent = name;
-    this._jobElement.textContent = job;
+    this._jobElement.textContent = about;
+    if (avatar) {
+      this.setAvatar(avatar);
+    }
+    if (_id) {
+      this._id = _id;
+    }
+  }
+
+  setAvatar(avatar) {
+    this._avatarElement.src = avatar;
   }
 }
